e3015e7de8849add0d4dd335cfa0b85a9baaf58c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-02-20 11:32:16 +0700

    Rename WordListU to WordListC, base on WordListC

--- lib/WordList.pm
+++ lib/WordList.pm
@@ -3,105 +3,9 @@ package WordList;
 # DATE
 # VERSION
 
-use strict 'subs', 'vars';
+use base 'WordListC';
 
-sub new {
-    my $class = shift;
-    my $fh = \*{"$class\::DATA"};
-    binmode $fh, "encoding(utf8)";
-    unless (defined ${"$class\::DATA_POS"}) {
-        ${"$class\::DATA_POS"} = tell $fh;
-    }
-    bless [], $class;
-}
-
-sub each_word {
-    my ($self, $code) = @_;
-
-    my $class = ref($self);
-
-    my $fh = \*{"$class\::DATA"};
-
-    seek $fh, ${"$class\::DATA_POS"}, 0;
-    while (defined(my $word = <$fh>)) {
-        chomp $word;
-        $code->($word);
-    }
-}
-
-sub pick {
-    my ($self, $n) = @_;
-
-    $n ||= 1;
-
-    my $class = ref($self);
-
-    my $fh = \*{"$class\::DATA"};
-
-    seek $fh, ${"$class\::DATA_POS"}, 0;
-    if ($n < 1) {
-        die "Please specify a positive number of words to pick";
-    } elsif ($n == 1) {
-        # use algorithm from Learning Perl
-        my $word;
-        my $i = 0;
-        while (defined(my $line = <$fh>)) {
-            $i++;
-            $word = $line if rand($i) < 1;
-        }
-        chomp($word);
-        return $word;
-    } else {
-        my @words;
-        my $i = 0;
-        while (defined(my $line = <$fh>)) {
-            $i++;
-            if (@words < $n) {
-                # we haven't reached $n, put word to result in a random position
-                splice @words, rand(@words+1), 0, $line;
-            } else {
-                # we have reached $n, just replace a word randomly, using
-                # algorithm from Learning Perl, slightly modified
-                rand($i) < @words and splice @words, rand(@words), 1, $line;
-            }
-        }
-        chomp(@words);
-        return @words;
-    }
-}
-
-sub word_exists {
-    my ($self, $word) = @_;
-
-    my $class = ref($self);
-
-    my $fh = \*{"$class\::DATA"};
-
-    seek $fh, ${"$class\::DATA_POS"}, 0;
-    while (defined(my $line = <$fh>)) {
-        chomp $line;
-        if ($word eq $line) {
-            return 1;
-        }
-    }
-    0;
-}
-
-sub all_words {
-    my ($self) = @_;
-
-    my $class = ref($self);
-
-    my $fh = \*{"$class\::DATA"};
-
-    seek $fh, ${"$class\::DATA_POS"}, 0;
-    my @res;
-    while (defined(my $word = <$fh>)) {
-        chomp $word;
-        push @res, $word;
-    }
-    @res;
-}
+# TODO: binary search method, etc
 
 1;
 # ABSTRACT: Word lists
@@ -207,8 +111,8 @@ Return all the words in a list.
 
 =head1 SEE ALSO
 
-L<WordListU> is just like L<WordList> except it does not impose a sorting order.
-You can sort the wordlist in whatever order you need.
+L<WordListC> is just like L<WordList> except it does not impose ascibetical
+sorting order requirement. You can sort the wordlist in whatever order you need.
 
 L<Bencher::Scenario::GamesWordlistModules>
 
