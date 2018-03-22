eeb67f839e26fbf9fca98e5a908bb183e1ba5825
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-03-22 18:48:28 +0700

    [test] Add some tests

---
+++ lib/WordList/Test/OneTwo.pm
@@ -0,0 +1,16 @@
+package WordList::Test::OneTwo;
+
+# DATE
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: Wordlist that contains "one" and "two"
+
+__DATA__
+one
+two
