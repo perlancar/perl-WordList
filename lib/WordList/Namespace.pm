8222c548beb345587570ce7d247952bed674cc78
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-03-22 18:35:12 +0700

    Define WordList_Modules & WordList_Namespaces

--- lib/WordList/Namespace.pm
+++ lib/WordList/Namespace.pm
@@ -9,11 +9,27 @@ use warnings;
 use Exporter 'import';
 our @EXPORT_OK = qw(is_actual_wordlist_module);
 
+our %WordList_Modules = (
+);
+
+our %WordList_Namespaces = (
+    'WordList::Char'            => 1,
+    'WordList::Phrase'          => 1,
+    'WordList::Password'        => 1,
+    'WordList::MetaSyntactic'   => 1,
+);
+
 our %Non_WordList_Modules = (
     'WordList'                  => 1,
     'WordList::Namespace'       => 1, # us!
     'WordList::MetaSyntactic'   => 1, # base class for WordList::MetaSyntactic::*
 );
+
+our $WordList_Namespaces_RE = join(
+    '|', map {quotemeta} sort {length($b) <=> length($a)}
+        keys %WordList_Namespaces);
+$WordList_Namespaces_RE =
+    qr/\A(?:$WordList_Namespaces_RE)(?:::|\z)/;
 
 our %Non_WordList_Namespaces = (
     'WordList::Role'            => 1,
@@ -29,12 +45,14 @@ sub is_actual_wordlist_module {
     my $mod = shift;
 
     $mod =~ /\AWordList::/ or return 0;
+    $WordList_Modules{$mod} and return 3;
     $Non_WordList_Modules{$mod} and return 0;
+    $mod =~ $WordList_Namespaces_RE and return 2;
     $mod =~ $Non_WordList_Namespaces_RE and return 0;
     1;
 }
 
-# ABSTRACT: List WordList::* namespaces
+# ABSTRACT: Catalog of WordList::* namespaces
 
 =head1 DESCRIPTION
 
